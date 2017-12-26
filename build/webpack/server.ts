d28f2f7d521b5efb02858944937f61a24755db18
Jiaren Liu <jrliu@alauda.io>
2017-12-26 15:38:40 +0800

    separate webpack server config

--- build/webpack/server.ts
+++ build/webpack/server.ts
@@ -1,26 +1,20 @@
 import * as fs from 'fs'
 
-import * as _debug from 'debug'
 import * as webpack from 'webpack'
-import * as merge from 'webpack-merge'
 import * as nodeExternals from 'webpack-node-externals'
 
-import { globals, paths } from '../config'
+import { paths } from '../config'
 
 import baseConfig from './base'
 
-const { NODE_ENV } = globals
-
-const debug = _debug('rubick:webpack:client')
-
-debug(`create webpack configuration for NODE_ENV:${NODE_ENV}`)
-
 const lernaModulesDir = paths.base('../../node_modules')
 
-export default merge.smart(baseConfig, {
+const config: webpack.Configuration = {
+  ...baseConfig,
   entry: paths.server('index.ts'),
   target: 'node',
   resolve: {
+    extensions: ['.ts', '.js'],
     modules: [paths.src(), paths.server('router'), 'node_modules'],
   },
   output: {
@@ -33,5 +27,11 @@ export default merge.smart(baseConfig, {
       ? lernaModulesDir
       : 'node_modules',
   }),
-  plugins: [new webpack.optimize.UglifyJsPlugin({ comments: false })],
-})
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false,
+    }),
+  ],
+}
+
+export default config

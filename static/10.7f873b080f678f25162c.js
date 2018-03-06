df067ec9609d887434aec41e647e7bcb1c3de075
JounQin <admin@1stg.me>
2018-03-06 16:16:38 +0000

    bump webpack@4 usage, add-asset-html-webpack-plugin not compatible

--- static/10.7f873b080f678f25162c.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([10],{95:function(e,l,t){"use strict";Object.defineProperty(l,"__esModule",{value:!0});var n=function(){var e=this,l=e.$createElement;return(e._self._c||l)("div")},u=[],r=t(4),c=Object(r.a)(null,n,u,!1,null,null,null);l.default=c.exports}});

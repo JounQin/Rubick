2fe3b2eb262e6bb02246692ae80035e7cea85134
JounQin <admin@1stg.me>
2018-03-11 12:24:35 +0000

    Update fonts.css to the latest version 🚀 (#48)

--- static/3.6ce37964ff7495e10ed3.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{27:function(n,e,t){"use strict";t.r(e);var l=t(1),u=Object(l.a)(null,function(){var n=this.$createElement;return(this._self._c||n)("div")},[],!1,null,null,null);e.default=u.exports}}]);

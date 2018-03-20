f4815af8cc50adee3fac058ace81496e08c7239f
JounQin <admin@1stg.me>
2018-03-20 01:24:13 +0000

    use @types/webpack-env to simplify types definition

---
+++ static/6.2c97b936001f7d3a78bb.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{23:function(t,e,n){"use strict";n.r(e);var i=n(1),l=Object(i.a)(null,function(){var t=this.$createElement;return(this._self._c||t)("router-link",{attrs:{to:"/image/image-repository/detail/123"}},[this._v("detail")])},[],!1,null,null,null);e.default=l.exports}}]);
